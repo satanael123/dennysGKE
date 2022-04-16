1c84b1472b257301fa348719718155f1b04ab7cb
satanael123 <103818347+satanael123@users.noreply.github.com>
2022-04-16 17:34:57 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,10 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Nama : Dennys Bunadi
-              NIM : 52019009
-              Jurusan : Teknik Informatika!\n')
-})
+    res.send('Nama : Dennys Bunadi NIM : 52019009 Jurusan : Teknik Informatika!\n')})
 
 app.listen(PORT, HOST)
 console.log(`Running on http://${HOST}:${PORT}`)
